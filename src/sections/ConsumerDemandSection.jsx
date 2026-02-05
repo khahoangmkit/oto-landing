82c2ee45693bf6aa09f2fa01220904d9a8289e70
hoangdk <hoangdk@viettelpost.com.vn>
2026-02-05 23:14:41 +0700

    add block new

--- src/sections/ConsumerDemandSection.jsx
+++ src/sections/ConsumerDemandSection.jsx
@@ -1,4 +1,11 @@
 import Reveal from '../components/Reveal'
+import tyLeChuyenDoi from '../assets/tylechuyendoi.png'
+import imgTitle from '../assets/thoithuc-title.png'
+import tocDoTitle from '../assets/thanhkhoan-title.png'
+import xeGeely from '../assets/car1.png'
+import xeOther from '../assets/car2.png'
+import calenIcon from '../assets/calen_1.png'
+
 
 export default function ConsumerDemandSection() {
   return (
@@ -6,24 +13,20 @@ export default function ConsumerDemandSection() {
       <div className="container1440">
         <div className="demandTop">
           <Reveal>
-            <div className="demandIllustration ph">
-              <div className="demandBadge">
-                <p className="demandBadge__label">Tỷ lệ chuyển đổi</p>
-                <p className="demandBadge__value">3.1%</p>
-              </div>
+            <div className="demandIllustration">
+              <img src={tyLeChuyenDoi} alt="ty le chuyen doi"/>
             </div>
           </Reveal>
 
           <div className="demandText">
             <Reveal delay={100}>
-              <p className="scriptTitle">HTAT thúc đẩy nhu cầu mua sắm</p>
-            </Reveal>
-            <Reveal delay={150}>
-              <h2 className="bigTitle">CỦA NGƯỜI TIÊU DÙNG</h2>
+              <img src={imgTitle} alt=""/>
             </Reveal>
             <Reveal delay={200}>
               <p className="paragraph">
-                Trung bình cứ 100 người tìm kiếm giá xe Geely trên Oto.com.vn, có khoảng 3 khách hàng muốn liên hệ trực tiếp đại lý, nhân viên bán hàng để tìm hiểu chi tiết về chính sách giá, khuyến mại và có mong muốn đến xem xe trực tiếp
+                Trung bình cứ 100 người tìm kiếm giá xe Geely trên Oto.com.vn, có khoảng 3 khách hàng muốn liên hệ trực
+                tiếp đại lý, nhân viên bán hàng để tìm hiểu chi tiết về chính sách giá, khuyến mại và có mong muốn đến
+                xem xe trực tiếp
               </p>
             </Reveal>
           </div>
@@ -31,19 +34,21 @@ export default function ConsumerDemandSection() {
 
         <Reveal delay={250}>
           <div className="liquidityBlock">
-            <h3 className="sectionHeading sectionHeading--center">TỐC ĐỘ THANH KHOẢN CỦA XE GEELY</h3>
-            <p className="subLine" style={{ textAlign: 'center' }}>TIỆM CẬN VỚI XE NHẬT, HÀN</p>
+            <img src={tocDoTitle} alt=""/>
           </div>
         </Reveal>
 
         <div className="compareGrid">
           <Reveal delay={300}>
             <div className="compareCol">
+              <img src={xeGeely} alt=""/>
               <p className="compareLabel">XE GEELY</p>
-              <div className="ph carPh"></div>
               <div className="miniCard">
-                <p className="miniCard__value">16,8</p>
-                <p>Ngày</p>
+                <img src={calenIcon} style={{width: '100%'}} alt=""/>
+                <div className='displayFlex flexCol cardTitle '>
+                  <p className="miniCard__value">16,8</p>
+                  <p className="paragraph">Ngày</p>
+                </div>
               </div>
             </div>
           </Reveal>
@@ -52,11 +57,16 @@ export default function ConsumerDemandSection() {
 
           <Reveal delay={350}>
             <div className="compareCol">
+              <img src={xeOther} alt=""/>
               <p className="compareLabel">XE NHẬT, HÀN</p>
               <div className="ph carPh"></div>
               <div className="miniCard">
-                <p className="miniCard__value">15,7</p>
-                <p>Ngày</p>
+                <img src={calenIcon} alt=""/>
+                <div className='displayFlex flexCol cardTitle'>
+                  <p className="miniCard__value">15,7</p>
+                  <p className='paragraph'>Ngày</p>
+                </div>
+
               </div>
             </div>
           </Reveal>
@@ -64,7 +74,10 @@ export default function ConsumerDemandSection() {
 
         <Reveal delay={400}>
           <p className="smallNote">
-            "Hành trình an tâm" đem đến những trải nghiệm thực tế, khách quan từ người dùng, qua đó giúp những người đã, đang và sắp có kế hoạch sở hữu xe Geely yên tâm về chất lượng sản phẩm. Minh chứng cho việc tốc độ thanh khoản của Geely đã qua sử dụng hiện 16,8 ngày, chỉ lâu hơn 1 ngày so với các đối thủ cùng phân khúc đến từ các thương hiệu truyền thống Nhật, Hàn.
+            "Hành trình an tâm" đem đến những trải nghiệm thực tế, khách quan từ người dùng, qua đó giúp những người đã,
+            đang và sắp có kế hoạch sở hữu xe Geely yên tâm về chất lượng sản phẩm. Minh chứng cho việc tốc độ thanh
+            khoản của Geely đã qua sử dụng hiện 16,8 ngày, chỉ lâu hơn 1 ngày so với các đối thủ cùng phân khúc đến từ
+            các thương hiệu truyền thống Nhật, Hàn.
           </p>
         </Reveal>
 
@@ -79,15 +92,15 @@ export default function ConsumerDemandSection() {
               <div className="barList">
                 <div className="barItem">
                   <span className="barItem__val">13,5%</span>
-                  <div className="barItem__bar" style={{ width: '100%' }}></div>
+                  <div className="barItem__bar" style={{width: '100%'}}></div>
                 </div>
                 <div className="barItem">
                   <span className="barItem__val">11,2%</span>
-                  <div className="barItem__bar barItem__bar--mid" style={{ width: '83%' }}></div>
+                  <div className="barItem__bar barItem__bar--mid" style={{width: '83%'}}></div>
                 </div>
                 <div className="barItem">
                   <span className="barItem__val">12,7%</span>
-                  <div className="barItem__bar barItem__bar--low" style={{ width: '94%' }}></div>
+                  <div className="barItem__bar barItem__bar--low" style={{width: '94%'}}></div>
                 </div>
                 <p className="barSource">Nguồn: oto.com.vn</p>
               </div>
@@ -101,12 +114,15 @@ export default function ConsumerDemandSection() {
               </Reveal>
               <Reveal delay={600}>
                 <p className="paragraph">
-                  Sau khoảng 6 tháng sử dụng, Geely Coolray giảm trung bình 13,5% so với giá xe mới lăn bánh, trong khi đó Hyundai Santa Fe và Ford Everest giảm khoảng 12,7%.
+                  Sau khoảng 6 tháng sử dụng, Geely Coolray giảm trung bình 13,5% so với giá xe mới lăn bánh, trong khi
+                  đó Hyundai Santa Fe và Ford Everest giảm khoảng 12,7%.
                 </p>
               </Reveal>
               <Reveal delay={650}>
                 <p className="paragraph">
-                  Sự chênh lệch này không đáng kể so với các đối thủ cùng phân khúc. Để có được sự thành công này, cũng có đóng góp không nhỏ tới từ HTAT khi khách hàng đã gạt bỏ được các rào cản về hình về tâm lý khi sử dụng xe TQ.
+                  Sự chênh lệch này không đáng kể so với các đối thủ cùng phân khúc. Để có được sự thành công này, cũng
+                  có đóng góp không nhỏ tới từ HTAT khi khách hàng đã gạt bỏ được các rào cản về hình về tâm lý khi sử
+                  dụng xe TQ.
                 </p>
               </Reveal>
             </div>
